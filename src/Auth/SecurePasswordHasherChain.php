82cac81057c1f7b465bf3bcb4034726fa150dd48
Tobias Derksen <tobias.derksen@codecentric.de>
2018-08-28 10:45:57 +0200

    Added password hasher chain

---
+++ src/Auth/SecurePasswordHasherChain.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lukeelten\PasswordHasher\Auth;
+
+use Cake\Auth\FallbackPasswordHasher;
+
+/**
+ * Class SecurePasswordHasherChain
+ * @package lukeelten\PasswordHasher\Auth
+ */
+class SecurePasswordHasherChain extends FallbackPasswordHasher
+{
+
+    /**
+     * SecureHasherChain constructor.
+     * @param array $config Initial configuration
+     */
+    public function __construct(array $config = [])
+    {
+        if (!array_key_exists("hashers", $config)) {
+            $config["hashers"] = [
+                Argon2PasswordHasher::class,
+                DefaultPasswordHasher::class,
+                BcryptPasswordHasher::class
+            ];
+        }
+
+        parent::__construct($config);
+    }
+}
---
+++ tests/TestCase/Auth/SecurePasswordHasherChainTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace lukeelten\PasswordHasherTest\Auth;
+
+use lukeelten\PasswordHasher\Auth\Argon2PasswordHasher;
+use lukeelten\PasswordHasher\Auth\BcryptPasswordHasher;
+use lukeelten\PasswordHasher\Auth\SecurePasswordHasherChain;
+
+class SecurePasswordHasherChainTest extends PasswordHasherTestBase
+{
+
+    protected static $className = SecurePasswordHasherChain::class;
+
+    public function testChain()
+    {
+        $instance = new SecurePasswordHasherChain();
+
+        $bcryptHasher = new BcryptPasswordHasher();
+        $argonHasher = new Argon2PasswordHasher();
+
+        $password = $this->randomPassword();
+
+        $hash = $instance->hash($password);
+        $this->assertTrue($instance->check($password, $hash));
+
+        // Password should have been hashed with argon2
+        $this->assertTrue($argonHasher->check($password, $hash));
+
+        if (!defined("PASSWORD_ARGON2I")) {
+            // Password should not have been hashed with bcrypt
+            // But this can only be checked if password_verify does not support argon2
+            $this->assertFalse($bcryptHasher->check($password, $hash));
+        }
+
+        // Hash with Bcrypt and check with chain
+        $hash = $bcryptHasher->hash($password);
+        $this->assertTrue($instance->check($password, $hash));
+    }
+}
